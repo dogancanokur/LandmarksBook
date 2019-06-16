8b4bd0e97dd8a9eebea9cc85cdba706ece014b16
dogancanokur <ahmet.okur.94@gmail.com>
2019-06-16 04:07:32 +0300

    add landmarks and landmarks photos convert to bitmap

--- app/src/main/java/com/dogancanokur/landmarkbook/MainActivity.java
+++ app/src/main/java/com/dogancanokur/landmarkbook/MainActivity.java
@@ -1,5 +1,7 @@
 package com.dogancanokur.landmarkbook;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -19,11 +21,27 @@ public class MainActivity extends AppCompatActivity {
 
         ArrayList<String> landmarkNames = new ArrayList<String>();
         landmarkNames.add("Pisa");
-        landmarkNames.add("Colleseo");
+        landmarkNames.add("Collesseum");
         landmarkNames.add("Eiffel");
+        landmarkNames.add("London Bridge");
+        landmarkNames.add("Leander's Tower");
 
         ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, landmarkNames);
-
         listView.setAdapter(arrayAdapter);
+
+        Bitmap pisa = BitmapFactory.decodeResource(this.getResources(), R.drawable.pisa);
+        Bitmap collesseum = BitmapFactory.decodeResource(this.getResources(), R.drawable.colosseum);
+        Bitmap eiffel = BitmapFactory.decodeResource(this.getResources(), R.drawable.eiffel);
+        Bitmap londonBridge = BitmapFactory.decodeResource(this.getResources(), R.drawable.londonbridge);
+        Bitmap leadensTower = BitmapFactory.decodeResource(this.getResources(), R.drawable.leanderstower);
+
+        ArrayList<Bitmap> landmarksImages = new ArrayList<Bitmap>();
+        landmarksImages.add(pisa);
+        landmarksImages.add(collesseum);
+        landmarksImages.add(eiffel);
+        landmarksImages.add(londonBridge);
+        landmarksImages.add(leadensTower);
+
+
     }
 }
